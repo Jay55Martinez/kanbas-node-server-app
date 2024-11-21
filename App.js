daef375c1a1fb609faa2305d6fae1a34ea58cf72
Jay55Martinez <martinez.jay@northeastern.edu>
2024-11-20 21:38:59 -0500

    completed 4.3

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 // const express = require('express');
+import "dotenv/config";
 import express from "express";
 import HelloRoutes from "./hello.js";
 import Lab5 from "./Lab5/index.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import cors from "cors";
+import session from "express-session";
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+}));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+app.use(session(sessionOptions));
 // req is all the information comming from the client
 // res represents the response of the server 
+UserRoutes(app);
 HelloRoutes(app);
 Lab5(app);
 
